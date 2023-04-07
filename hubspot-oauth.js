5e2c38d0a6dc09df0a484cb82cf9b38c1ecb9cf9
Nicolas Leon <nicolasleon@nicolasc3mxq05n.myfiosgateway.com>
2023-04-06 21:42:54 -0400

    first commit

---
+++ hubspot-oauth.js
@@ -0,0 +1,108 @@
+const axios = require('axios');
+const queryString = require('querystring');
+
+
+///////////////////////////////////////////////////////////////////////////////
+// oath.js
+//  Use:  Includes OAuth authentication functions for HubSpot
+//  
+//  Date:
+//  Change log:
+//
+///////////////////////////////////////////////////////////////////////////////
+
+//Constants
+const redirectURI = 'https://sbm-hs-oauth.herokuapp.com/oath-callback';  //This should probably be an environment var
+const authURL = 'https://api.hubapi.com/oauth/v1/token';
+
+//getNewToken
+//Input:  HubSpot code (required)
+//Output:  OAuth token object
+const getNewToken = (oauthCode) => {
+    //return a promise so you can use async/await if needed
+    return new Promise(async (token) => {
+        if (oauthCode === null) {
+            //Throw error, code is required
+            const err = new Error('HubSpot code is missing');
+            throw err;
+        }
+
+        var responseToken = {};
+        //var url = authURL + '&code=' + oauthCode;
+
+        var config = {
+            headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'},
+            responseType: 'blob'
+        };
+        
+        //Used to get authorization code
+        const formData = {
+            grant_type: 'authorization_code',
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            redirect_uri: redirectURI,
+            code: oauthCode
+        };
+
+        const data = queryString.stringify(formData);
+
+        console.log('Calling OAuth for token');
+        await axios.post(authURL, data, config)
+        .then(function (response) {
+            responseToken = response.data;
+        })
+        .catch(function (error) {
+            console.log('Error in calling OAuth: ' + error);
+            console.log(error.response);
+            throw error;
+        });
+        
+        token(responseToken);
+    });
+}   
+
+//refreshToken
+//Input:  oathToken which is the refresh token from the original call (required)
+//Output:  new Oauth token object
+const refreshToken = (oathToken) => {
+    //return a promise so you can use async/await if needed
+    return new Promise(async (token) => {
+        if (oauthToken === null) {
+            //Throw error, code is required
+            const err = new Error('HubSpot code is missing');
+            throw err;
+        }
+
+        //Header configuration
+        var config = {
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            responseType: 'blob'
+        };
+
+        //Used to get authorization code
+        var formData = {
+            grant_type: 'refresh_token',
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            redirect_uri: redirectURI,
+            refresh_token: oauthToken
+        };
+
+        var responseToken = {};
+
+        console.log('Calling OAuth for token');
+        await axios.post(url, {form: formData}, config)
+        .then(function (response) {
+            responseToken = response.data;
+        })
+        .catch(function (error) {
+            console.log('Error in calling OAuth: ' + error);
+            throw error;
+        });
+        
+        token(responseToken);
+    });
+} 
+
+module.exports.refreshToken = refreshToken;
+module.exports.newToken = getNewToken;
---
+++ server.js
@@ -0,0 +1,57 @@
+//required installed modules
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const crypt =require('crypto');  //used to validate HubSpot request
+const querystring = require('querystring');
+
+//declare custom modules
+const hsOauth = require('./hubspot-oauth');
+
+ //declare variables from modules
+const app = express();  //used to start web app
+
+//Endpoints
+//oath-callback:  Used in the Oath validation process to 
+app.get('/oath-callback', async(req, res) => {
+    
+    //verify code is included
+    if (req.query.code) {
+        try {
+            console.log("Attempting to generate new token");
+            token = await hsOauth.newToken(req.query.code);
+        }
+        catch (err) {
+            console.log('Error caught in getting OAuth token: ' + err);
+            return res.redirect('/error?msg=Error%20Authorizing%20App');
+        }
+    }
+
+    
+    console.log('access token = ' + token.access_token);
+    res.redirect('/readme');
+    
+
+}); 
+
+//Error page
+app.get('/error', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`<h4>Error:  ${req.query.msg}`);
+    res.end();
+});
+
+app.get('/readme', (req, res) => {
+    res.setHeader('Content-Type','text/html');
+    res.write('<h4>Congrats, the app is installed</h4>');
+    res.end();
+});
+
+//Listner
+app.listen(process.env.PORT, () => console.log('listening on ' + process.env.PORT));
+
+
+
+
+
+
